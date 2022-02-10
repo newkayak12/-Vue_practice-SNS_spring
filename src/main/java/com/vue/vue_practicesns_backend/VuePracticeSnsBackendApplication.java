3d5b781430a0ede984a787ee2a54014622595018
newkayak12 <newkayak12@gmail.com>
2022-02-10 22:52:39 +0900

    init

---
+++ src/main/java/com/vue/vue_practicesns_backend/VuePracticeSnsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vue.vue_practicesns_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuePracticeSnsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuePracticeSnsBackendApplication.class, args);
+	}
+
+}
